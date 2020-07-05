46ce4fdfe375909bc5a675f42f96bbd923df3a36
Mainsector <67473566+Mainsector@users.noreply.github.com>
2020-07-05 08:42:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -31,7 +31,6 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "mainsector.cf",
   "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
